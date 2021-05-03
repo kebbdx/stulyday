d21f93c5dbc3b1eea604eb6a183a936520ba619a
kebbdx <bensalah.kamelleddine@gmail.com>
2021-05-03 09:58:34 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+//(function($){
+
+    //$('.fa-caret-square-down').hover(function(){
+      //  console.log("test"); 
+        //$(".menu").slideToggle();
+   
+  ///  });
+
+    
+    
+// //     $( ".fa-caret-square-down")
+// //   .odd().hide().end().even().hover(function() {
+    
+// //         $( this )
+// //                 .toggleClass( "active" )
+// //                 .ul
+// //                 .stop( true, true )
+// //                 .slideToggle();
+//   });
+
+//})(jQuery);
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    
+
+    <title>Document</title>
+</head>
+<body>
+
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                     
+                      
+                    </ul>
+                    
+                  </div>
+                </div>
+              </nav>
+
+        </header>
+    
+   
+    
+
+        
+
+
+
+        <script src="/assets/lib/jquery-3.6.0.min.js"></script>
+        <script src="./assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/script.js"></script>
+
+</body>
+</html>
